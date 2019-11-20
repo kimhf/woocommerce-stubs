74a50ddab6c6a8acc1a2f1c8a13b530f4ff8a05e
Viktor Szépe <viktor@szepe.net>
2019-11-21 00:26:34 +0100

    Implement idea

--- finder.php
+++ finder.php
@@ -3,7 +3,7 @@
 return \StubsGenerator\Finder::create()
     ->in([
         'wp-content/plugins/woocommerce/includes',
-        'wp-content/plugins/woocommerce/packages/*/src',
+        // 'wp-content/plugins/woocommerce/packages/*/src', TODO Put packages into separate stubs files.
     ])
     ->append(\StubsGenerator\Finder::create()->files()->depth('< 1')->name('woocommerce.php')->in(['woocommerce']))
     ->sortByName();
