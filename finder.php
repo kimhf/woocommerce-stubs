359506cbfd8cd458ac923d703459bb17c940bb72
Viktor Szépe <viktor@szepe.net>
2019-11-20 02:47:04 +0100

    append() work only this way

--- finder.php
+++ finder.php
@@ -2,8 +2,8 @@
 
 return \StubsGenerator\Finder::create()
     ->in([
-        'wp-content/plugins/woocommerce/woocommerce.php',
         'wp-content/plugins/woocommerce/includes',
         'wp-content/plugins/woocommerce/packages/*/src',
     ])
+    ->append(\StubsGenerator\Finder::create()->files()->depth('< 1')->name('woocommerce.php')->in(['woocommerce']))
     ->sortByName();
